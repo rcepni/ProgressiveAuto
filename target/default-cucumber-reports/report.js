befc3fbf8d144a9bf137cbe2c979679a2a5b4cdc
Riza <rizacepni@gmail.com>
2018-10-15 23:47:30 -0400

    first commit

---
+++ target/default-cucumber-reports/report.js
@@ -0,0 +1,488 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/autoRegister.feature");
+formatter.feature({
+  "name": "Insurance Quote from Progressive",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@progressive"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Insurance quote for one person",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@progressive"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user is on the homepage of Progressive",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_is_on_the_homepage_of_Progressive()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user verifies url and title while on the homepage.",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_verifies_url_and_title_while_on_the_homepage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on the Auto button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_on_the_Auto_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enters the zip code",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_enters_the_zip_code()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on the Quote Auto button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_on_the_Quote_Auto_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enters the first name",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_enters_the_first_name()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enters the last name",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_enters_the_last_name()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enters the address",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_enters_the_address()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enters the city",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_enters_the_city()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enters the date of birth",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_enters_the_date_of_birth()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on the Okay, start my quote button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_on_the_Okay_start_my_quote_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user chooses the year of the vehicle",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_chooses_the_year_of_the_vehicle()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user chooses the make of the vehicle",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_chooses_the_make_of_the_vehicle()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user chooses the model of the vehicle",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_chooses_the_model_of_the_vehicle()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects body type",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_body_type()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects the purpose of use",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_the_purpose_of_use()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user selects if the car is owned or leased",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.user_selects_if_the_car_is_owned_or_leased()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects for how lond the car has been owned",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_for_how_lond_the_car_has_been_owned()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on the Done button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_on_the_Done_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on the Save and Continue button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_on_the_Save_and_Continue_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user chooses the gender",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_chooses_the_gender()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects marital status",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_marital_status()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects the highest level of education",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_the_highest_level_of_education()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects employment status",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_employment_status()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user views the accupation list",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_views_the_accupation_list()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects an industry from the industry selection list",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_an_industry_from_the_industry_selection_list()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects an occupation from the occupation list",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_an_occupation_from_the_occupation_list()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on the save and continue button after selecting occupation",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_on_the_save_and_continue_button_after_selecting_occupation()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects primary residence",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_primary_residence()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user answers the question if s/he moved in two years",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_answers_the_question_if_s_he_moved_in_two_years()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects the years licensed",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_the_years_licensed()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user answers the question if s/he was involven in an accident",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_answers_the_question_if_s_he_was_involven_in_an_accident()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user reports the violations and tictets",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_reports_the_violations_and_tictets()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on the save and continue button and continues",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_on_the_save_and_continue_button_and_continues()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on the fourth save and continue button to continue",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_on_the_fourth_save_and_continue_button_to_continue()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on the save and continue button for the fifth time",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_on_the_save_and_continue_button_for_the_fifth_time()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user answers the question about insurance history",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_answers_the_question_about_insurance_history()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects the duration with s/he is insured with the current company",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_the_duration_with_s_he_is_insured_with_the_current_company()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user selects most recent bodily injury limits",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_selects_most_recent_bodily_injury_limits()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user chooeses if s/he has purchases a service from progressive other than car insurance",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_chooeses_if_s_he_has_purchases_a_service_from_progressive_other_than_car_insurance()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user chooses if s/he was insured by progressive last month",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_chooses_if_s_he_was_insured_by_progressive_last_month()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enters the primary email address",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_enters_the_primary_email_address()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user answers the question if the family members have health insurance",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_answers_the_question_if_the_family_members_have_health_insurance()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks oon the sixth save and continue button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_user_clicks_oon_the_sixth_save_and_continue_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the customer is asked if s/he wants to use snatpshot",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_customer_is_asked_if_s_he_wants_to_use_snatpshot()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the customer clicks on the save and continiue button for the seventh time",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "autoRegisterStepDefs.the_customer_clicks_on_the_save_and_continiue_button_for_the_seventh_time()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
